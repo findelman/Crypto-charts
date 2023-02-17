551f75a5c4632ac276537964414c462f16f44664
Alisher <alishcashq@gmail.com>
2023-02-17 15:55:21 +0600

    e

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,12 +2,13 @@ import store from "@/store";
 import { GlogalStyle } from "@/styles/GlogalStyle";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <GlogalStyle />
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   );
 }
