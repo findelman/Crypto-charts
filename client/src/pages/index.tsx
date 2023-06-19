b98b83bbee1f31f84aca3cc4b84d6a816e28411f
Alisher <alishcashq@gmail.com>
2023-06-20 04:30:04 +0600

    update

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,27 @@
 import { fetcher } from "@/api/fetcher";
 import { ChartWrapper } from "@/components/Chart/ChartWrapper";
-import useSWR from 'swr';
+import useSWR from "swr";
 import { CryptoList } from "@/components/PopularList/CryptoList";
 import Head from "next/head";
 import styled from "styled-components";
 
 const Wrapper = styled.div``;
 
-
-
-export default function Home({cryptoList}: any) {
+export default function Home({ cryptoList }: any) {
+  const testCryptoList = [
+    {
+      id: "bitcoin",
+      name: "Bitcoin",
+      current_price: 50000,
+      market_cap: 100000000,
+    },
+    {
+      id: "ethereum",
+      name: "Ethereum",
+      current_price: 3000,
+      market_cap: 50000000,
+    },
+  ];
   return (
     <>
       <Head>
@@ -17,16 +29,16 @@ export default function Home({cryptoList}: any) {
       </Head>
       <Wrapper>
         <ChartWrapper />
-        <CryptoList cryptoList={cryptoList}/>
+        <CryptoList cryptoList={cryptoList} />
       </Wrapper>
     </>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   try {
-    const response = await useSWR('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1', fetcher);
-    const cryptoList = response.data;
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1");
+    const cryptoList = await response.json();
     return {
       props: {
         cryptoList,
